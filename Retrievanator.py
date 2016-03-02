fd4455c9a6bd290f4cc5d36d915e84358d8c26ec
Nicholas Recht <nicholas.recht@gmail.com>
2016-03-02 15:07:25 -0700

    Updated the retrievanator to include detailed info

--- Retrievanator.py
+++ Retrievanator.py
@@ -5,8 +5,8 @@ import unicodecsv as csv
 # eww globals
 api_key = "46162c243979ee401c3e6942765f3d51"
 start_year = 2010
-end_year = 2016
-page_max = 21
+end_year = 2011
+page_max = 2
 request_limit = 5
 
 
@@ -31,8 +31,23 @@ def get_movie_list():
                 print(url)
     return movie_list
 
-get detailed_movie_info(id):
 
+def detailed_movie_info(id):
+    url = "https://api.themoviedb.org/3/movie/" + str(id) + "?api_key=" + api_key
+    request_count = 0
+    r = requests.get(url=url)
+    while request_count < request_limit and r.status_code != 200:
+        r = requests.get(url=url)
+        request_count += 1
+
+    return r.json()
+
+
+def get_required_fields(obj):
+    return [obj["id"], obj["title"], obj["adult"], obj["original_language"],
+                "|".join(sorted([x["name"] for x in obj["genres"]])),
+                "|".join(sorted([x["name"] for x in obj["production_companies"]])),
+                obj["budget"], obj["release_date"], obj["runtime"], obj["revenue"]]
 
 
 def write_to_csv(file_name, data):
@@ -45,7 +60,13 @@ def write_to_csv(file_name, data):
 def main(args):
     movies = get_movie_list()
     movies_arr = [[x["id"], x["title"]] for x in movies]
-    write_to_csv("move_list.csv", movies_arr)
+
+    detailed_list = []
+
+    for mov in movies_arr:
+        detailed_list.append(get_required_fields(detailed_movie_info(mov[0])))
+
+    write_to_csv("detailed_list.csv", detailed_list)
 
 
 if __name__ == "__main__":
